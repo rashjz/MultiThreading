8f3f4db7a0d7bd3bcaa222e25e3d761733c2fc35
Rashad Javadov <rashjz@users.noreply.github.com>
2017-12-28 14:20:15 +0100

    Create TaskServiceImpl.java

---
+++ multithreadingjavaapp/src/main/java/rashjz/info/app/TaskServiceImpl.java
@@ -0,0 +1,49 @@
+package rashjz.info.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.*;
+
+public class TaskServiceImpl {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskServiceImpl.class);
+
+    private ExecutorService executor = Executors.newFixedThreadPool(2);
+    private Semaphore semaphore = new Semaphore(2, true);//fairness true
+    BlockingQueue queue = new ArrayBlockingQueue(1024);
+
+    public void test() throws Exception {
+        executor.execute(() -> {
+            try {
+
+                queue.add("test1");
+                LOGGER.info("Queue size 1 "+queue.size());
+                LOGGER.info("task do some actions "+Thread.currentThread().getName());
+            } catch (Exception e) {
+                LOGGER.error("Error occurred while task submission", e);
+                e.printStackTrace();
+            }
+        });
+
+
+        Future future =   executor.submit(() -> {
+            try {
+                Thread.sleep(1000);
+
+                semaphore.acquire(1);
+                queue.add("test2");
+                LOGGER.info("task do another action "+Thread.currentThread().getName());
+                semaphore.release();
+
+                LOGGER.info("Queue size 2 "+queue.size());
+            } catch (Exception e) {
+                LOGGER.error("Error occurred while task submission", e);
+                e.printStackTrace();
+            }
+        });
+
+        executor.shutdown();
+    }
+
+
+}
